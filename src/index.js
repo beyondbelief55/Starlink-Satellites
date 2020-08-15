c5aaece8aef8f8ead17d7b313118be5960c244c0
beyondbelief55 <richardzhang858@gmail.com>
2020-08-15 02:46:43 -0700

    updated comments and css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-
+// this is the entrance of the javascript
 ReactDOM.render(
 
     <App />,
